b9699867ba3ca7f5ca130a60f05e440df88b9e7d
mex <javareturnclannadandairisnot0@gmail.com>
2023-06-03 10:17:28 +0800

    index.html无法渲染商品列表

---
+++ js/index_tool.js
@@ -0,0 +1,93 @@
+/**
+ * 检查密码强度,返回等级,0-9
+ * 长度: 4 6 8 10 12 16 20 20+
+ * 分级: 0 1 2  3  4  5  6  7
+ * 
+ * 每个类型加1分，如果密码长度>=12，加1分
+ * 
+ * 包含4种或4种以上不同类型的字符，密码长度>=8，加1分
+ * 
+ * 不能包含5个字符以上明显连续的小写(或大写)字母/数字，在分级中减1点
+ * 
+ * 若同时包含小写和大写字母和数字，则分级增加1点
+ * 
+ * @param {*} password 密码
+ * @returns 等级
+ */
+function checkPasswordStrength(password) {
+    var level = 0;
+    var regexArr = [/[a-z]/, /[A-Z]/, /[0-9]/, /[^a-zA-Z0-9]/];
+    var len = password.length;
+    
+
+    if (len < 4) {
+        level = 0;
+    } else if (len > 3 && len < 6) {
+        level = 1;
+    } else if (len >= 6 && len < 8) {
+        level = 2;
+    } else if (len >= 8 && len < 10) {
+        level = 3;
+    } else if (len >= 10 && len < 12) {
+        level = 4;
+    } else if (len >= 12 && len < 16) {
+        level = 5;
+    } else if (len >= 16 && len < 20) {
+        level = 6;
+    } else if (len >= 20) {
+        level = 7;
+    }
+
+    var typeCount = 0;
+    for (var i = 0; i < regexArr.length; i++) {
+        if (regexArr[i].test(password)) {
+            typeCount++;
+        }
+    }
+
+    if (typeCount > 2 && len >= 12) {
+        level++;
+    }
+
+    if (typeCount > 3 && len >= 8) {
+        level++;
+    }
+
+    if (/[a-z]{5,}/.test(password) || /[A-Z]{3,}/.test(password)) {
+        level--;
+    }
+
+    if (password.match(/[a-z]+/g) && password.match(/[A-Z]+/g) && password.match(/\d+/g)) {
+        level++;
+    }
+
+    if ( level < 0 ) { level = 0;}
+
+    if ( level > 9 ) { level = 9;}
+
+    return level;
+}
+
+/**
+ * 红->黄->绿 等分颜色, 返回颜色数组
+ * @param {*} num 份数
+ */
+function bisectionColor( num ) {
+    let colors = [];
+    let r, g, b;
+    let mid = parseInt(num/4);
+    for (var i = 0; i <= num; i++) {
+        if (i < mid) {
+            r = 255;
+            g = i * 51;
+            b = 0;
+        } else {
+            r = 255 - (i - 5) * 51;
+            g = 255;
+            b = 0;
+        }
+    var rgbValue = "rgb(" + r + "," + g + "," + b + ")";
+    colors.push(rgbValue);
+    }
+    return colors;
+}
---
+++ js/index_topBar_game.js
@@ -0,0 +1,49 @@
+const canvas = document.getElementById('canvas');
+const topbar = document.getElementById('topBar');
+const ctx = canvas.getContext('2d');
+const canvasWidth = canvas.width = topbar.offsetWidth;
+const canvasHeight = canvas.height = topbar.offsetHeight;
+
+var bird = new Image();
+bird.src = 0;
+var birdIndex = 0;
+var birdWigth = 52;
+var birdHeight = 28;
+var birdX = 0;
+var birdY = 0;
+var birdMoveSpeed = 5;
+
+function drawbird() {
+    birdIndex++;
+    if (birdIndex > 10) { birdIndex = 0; }
+    bird.src = "media/bird/skeleton-animation_"+birdIndex.toString().padStart(2, '0')+".png";
+    ctx.clearRect(birdX-birdMoveSpeed, birdY-birdMoveSpeed, birdWigth+birdMoveSpeed*2, birdHeight+birdMoveSpeed*2);
+    ctx.drawImage(bird, birdX, birdY, birdWigth, birdHeight);
+    requestAnimationFrame(drawbird);
+}
+
+// 添加键盘监听,监听上下左右箭头,调整bird位置
+document.addEventListener('keydown', function (event) {
+    switch (event.keyCode) {
+        case 37: // left
+            if(birdX > 0)
+                birdX -= birdMoveSpeed;
+            break;
+        case 38: // up
+            if(birdY > 0)
+            birdY -= birdMoveSpeed;
+            break;
+        case 39: // right
+            if(birdX < canvasWidth - birdWigth)
+                birdX += birdMoveSpeed;
+            break;
+        case 40: // down
+            if(birdY < canvasHeight - birdHeight)
+            birdY += birdMoveSpeed;
+            break;
+    }
+}, false);
+
+function topBargame() {
+    drawbird();
+}
